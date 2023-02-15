66893053a4056a9591f68166da59e2f253707510
ÖZLEM TEKİN <tekinozlem23@gmail.com>
2023-02-15 23:51:05 +0300

    named slot

---
+++ force-app/main/default/lwc/slotNamedPractice/slotNamedPractice.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class SlotNamedPractice extends LightningElement {}
--- force-app/main/default/lwc/slotParent/slotParent.js
+++ force-app/main/default/lwc/slotParent/slotParent.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
 
-export default class SlotParent extends LightningElement {}
+export default class SlotParent extends LightningElement {
+    country;
+    city;
+    famousFor;
+    handleChange(event){
+        const field = event.target.name;
+        if(field==="country"){
+            this.country=event.target.value;
+        }
+        if(field==="city"){
+            this.city=event.target.value;
+        }
+        if(field==="famous"){
+            this.famousFor=event.target.value;
+        }
+    }
+}
