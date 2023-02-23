bab0c17407d5072d8289a4e3f837e50fd45f9e2d
ÖZLEM TEKİN <tekinozlem23@gmail.com>
2023-02-23 11:49:09 +0300

    shallow update

--- force-app/main/default/lwc/shallowCopyObj/shallowCopyObj.js
+++ force-app/main/default/lwc/shallowCopyObj/shallowCopyObj.js
@@ -1,57 +1,81 @@
 import { LightningElement, track } from 'lwc';
 
 export default class ShallowCopyObj extends LightningElement {
-    // 1.Ornek: shallow copy of an object
-   @track originalObj = {
-        name:"Laura",
-        lastName:"Parson",
-        age:56,
-        address:{street:"ABC",
-            city:"Dallas",
-            state:"CA"}
-            };
-    //shallow copy of original obj
-    copyObj = {...this.originalObj};
 
-    handleChange(){
-        //copyObj modifiye ettik
-        this.copyObj.name = "Merry";
-        this.copyObj.age = 34;
-        this.copyObj.address.street ="Street 123";
-        console.log(this.copyObj.name);//Merry
-        console.log(this.originalObj.name);//Laura
+
+     // 1.Ornek
+
+     originalObj = {
+        name: 'Laura Parson',
+        age: 45,
+        address: {
+          street: '123 Maden Sk',
+          city: 'City',
+          state: 'CA'
+        }
+      };
+      
+      // Shallow copy the original object
+        copyObj = { ...this.originalObj };
+      
+       handleChanging(){
+    // Copy object uzerinde bir name property guncelle
+        this.copyObj.name = 'ozlem';
+    // Copy object uzerinde nested property guncelle
+        this.copyObj.address.street = '123 Elmali Sk';
+        console.log(this.originalObj.name); 
+        console.log(this.copyObj.name);  
     }
 
-    //2. ornek: get ve set ile shallow copy of an object
-     personOrj = {
-        name:"Ozge",
-        age:39
+
+   //2.ornek: get ve set ile shallow copy olusturma
+        @track
+         _person = {
+            name: 'John Doe',
+            age: 30
+        };
+    
+        // Getter for person object
+        get person() {
+            // original objectin modifiye edilmemesi icin shallow copy olustur
+            return { ...this._person };
+        }
+    
+        // Setter for person object
+        set person(value) {
+            // Shallow copy degerini al, value aktar
+            this._person = { ...value };
+        }
+    
+        handleNameChange(event) {
+            // person objectin shallow kopyasını al
+            const person1 = { ...this.person };
+            // Name property kullanicinin girdigi degerle update et
+            person1.name = event.target.value;
+            // Setten gelen degere aktar
+            this.person = person1;
+            this.orgPersonGoster();
+            // logOriginalPerson methodu ile orginal degerlerin oldugu methodu cagiriyoruz.
+
+        }
+    
+        handleAgeChange(event) {
+            // person objectinin shallow kopyasini getir. 
+            const person1 = { ...this.person };
+            // Yasi update et
+            person1.age = event.target.value;
+            // Setten gelen degere aktar
+            this.person = person1;
+           this.orgPersonGoster();
+             // logOriginalPerson methodu ile orginal degerlerin oldugu methodu cagiriyoruz.
+      }        
+
+      orgPersonGoster() {
+            //Yukarida update islemi yapılmadan once original versiyonu gozlemleyebiliriz.c
+            const originalPerson = { ...this._person };
+            console.log('Original name:', this._person.name);
+            console.log('Original age:', this._person.age);
+        }
     }
 
-    get person(){
-        // personOrj objectini cektik. 
-        return { ...this.personOrj};
-    }
-
-    set person(value){
-        //personOrj objesinin kopyasını person icerisine aktardik. 
-       const personOrj = {...value}; 
-       //encapsulation var. 
-    }
-
-    handleName(event){
-        //personOrj kopyasini olstur
-    const person ={...this.person};
-   this.person.name = event.target.value;
-   //this.person = personOrj;
-   console.log(this.personOrj.name);
-    }
-
-    handleAge(event){
-    const person = {...this.person};
-    person.age = event.target.value;
-   //this.age = person.age;
-    console.log(this.personOrj.age);
-}
-
-}
+      
